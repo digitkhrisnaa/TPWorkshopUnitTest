43663ab8c4050c40a1910a3c5a760331d3ce5c5c
digital.aurum <digital.aurum@BA-00019889.local>
2021-11-17 20:00:05 +0700

    add even and number formatter function

--- TPWorkshopUnitTest/ViewController.swift
+++ TPWorkshopUnitTest/ViewController.swift
@@ -11,9 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    func isEven(number: Int) -> Bool {
+        if number % 2 == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func numberFormatter(number: Int, separator: String = ".", currency: String = "Rp") -> String {
+        // looping dari belakang, per tiga karakter kasih titik
+        // 345 -> 543
+        let arrayNumbers = String(number).map { String($0) }
+        var count = 0
+        var result: [String] = []
+        for index in stride(from: arrayNumbers.count, to: 0, by: -1) {
+            count += 1
+            result.insert(arrayNumbers[index - 1], at: 0)
+            if count == 3 && index != 1 {
+                result.insert(separator, at: 0)
+                count = 0
+            }
+        }
+        
+        return "\(currency) \(result.joined())"
+    }
 }
 
