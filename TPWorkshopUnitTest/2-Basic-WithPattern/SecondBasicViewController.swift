eb94321333e3bc276a61f45b352d9350132f4f63
digitkhrisnaa <digital.aurum@tokopedia.com>
2021-11-18 19:45:30 +0700

    MVVM + Unit Test

--- TPWorkshopUnitTest/2-Basic-WithPattern/SecondBasicViewController.swift
+++ TPWorkshopUnitTest/2-Basic-WithPattern/SecondBasicViewController.swift
@@ -15,21 +15,29 @@ class SecondBasicViewController: UIViewController {
     @IBOutlet weak var numberFormatterTextField: UITextField!
     @IBOutlet weak var numberFormatterResultLabel: UILabel!
     
+    var viewModel = SecondBasicViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Second Basic"
-        // Do any additional setup after loading the view.
+        numberFormatterTextField.delegate = self
+        
+        viewModel.resultOddEven = { [weak self] result in
+            self?.oddEvenResultLabel.text = result
+        }
+        
+        viewModel.resultNumberFormatter = { [weak self] result in
+            self?.numberFormatterResultLabel.text = result
+        }
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func didTapFindOddEven(_ sender: Any) {
+        viewModel.isEven(text: oddEvenTextField.text)
     }
-    */
+}
 
+extension SecondBasicViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        viewModel.numberFormatter(text: textField.text)
+    }
 }
--- TPWorkshopUnitTestTests/2-Basic-WithPattern/2-Basic-WithPattern.swift
+++ TPWorkshopUnitTestTests/2-Basic-WithPattern/2-Basic-WithPattern.swift
@@ -10,24 +10,257 @@ import XCTest
 
 class __Basic_WithPattern: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var viewModel: SecondBasicViewModel!
+    override func setUp() {
+        viewModel = SecondBasicViewModel()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    /*
+        testing function odd or even
+     */
+    func test_isEven_notNumber() {
+        let expect = expectation(description: "check if isEven function is not number")
+        viewModel.resultOddEven = { result in
+            XCTAssertEqual(result, "Please put number")
+            expect.fulfill()
         }
+        
+        viewModel.isEven(text: "asdf")
+        waitForExpectations(timeout: 1, handler: nil)
     }
-
+    
+    func test_isEven_odd() {
+        let expect = expectation(description: "check if isEven returning Ganjil")
+        viewModel.resultOddEven = { result in
+            XCTAssertEqual(result, "Ganjil")
+            expect.fulfill()
+        }
+        
+        viewModel.isEven(text: "33")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_isEven_even() {
+        let expect = expectation(description: "check if isEven returning Genap")
+        viewModel.resultOddEven = { result in
+            XCTAssertEqual(result, "Genap")
+            expect.fulfill()
+        }
+        
+        viewModel.isEven(text: "32")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    /*
+        testing function number formatter
+     */
+    
+    func text_numberFormatter_notNumber() {
+        let expect = expectation(description: "number formatter not number")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Please put number")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "asdf")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_zero() {
+        let expect = expectation(description: "number formatter return zero")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 0")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "0")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_number() {
+        let expect = expectation(description: "number formatter return number")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 3")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "3")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_tens() {
+        let expect = expectation(description: "number formatter return tens")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 84")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "84")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_tens_withZero() {
+        let expect = expectation(description: "number formatter return tens with zero")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 10")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "10")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_hundred() {
+        let expect = expectation(description: "number formatter return hundred")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 639")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "639")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_hundred_withZero() {
+        let expect = expectation(description: "number formatter return hundred with zero")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 101")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "101")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_thousand() {
+        let expect = expectation(description: "number formatter return thousand")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 5.623")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "5623")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_thousand_withZero() {
+        let expect = expectation(description: "number formatter return thousand with zero")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 1.001")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "1001")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_tens_thousand() {
+        let expect = expectation(description: "number formatter return tens thousand")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 32.182")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "32182")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_tens_thousand_withZero() {
+        let expect = expectation(description: "number formatter return tens thousand with zero")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 10.021")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "10021")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_hundred_thousand() {
+        let expect = expectation(description: "number formatter return hundred thousand")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 932.175")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "932175")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_hundred_thousand_withZero() {
+        let expect = expectation(description: "number formatter return hundred thousand with zero")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 100.021")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "100021")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_million() {
+        let expect = expectation(description: "number formatter return million")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 3.828.232")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "3828232")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_tens_million() {
+        let expect = expectation(description: "number formatter return tens million")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 83.512.390")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "83512390")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_hundred_million() {
+        let expect = expectation(description: "number formatter return hundred million")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 128.312.852")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "128312852")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_billion() {
+        let expect = expectation(description: "number formatter return billion")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 9.238.912.932")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "9238912932")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_currency() {
+        let expect = expectation(description: "number formatter return currency")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "$ 1.000")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "1000", currency: "$")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_numberFormatter_separator() {
+        let expect = expectation(description: "number formatter return separator")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "SGD 1,000")
+            expect.fulfill()
+        }
+        
+        viewModel.numberFormatter(text: "1000", separator: ",", currency: "SGD")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
 }
